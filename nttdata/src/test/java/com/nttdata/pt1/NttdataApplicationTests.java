aaafa87170f201b0ccb3aac43cf3b46699199516
Eduardo Alberto Fernandez Bustamante <eduardo.afernandez@neoris.com>
2024-03-11 22:16:25 -0500

    :sparkles: creamos proyecto nttdata

---
+++ nttdata/src/test/java/com/nttdata/pt1/NttdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.pt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
